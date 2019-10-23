19631abddb28017d1e3036eb1f8c0b8d8bb419af
David Pomerenke <david.pomerenke@uni-konstanz.de>
2019-10-23 18:06:39 +0100

    add simple reflex agent

---
+++ intelligentAgents/simpleReflexAgent.mjs
@@ -0,0 +1,39 @@
+export class SimpleReflexAgent {
+  constructor (rules) {
+    this.rules = rules
+  }
+
+  action (percept) {
+    const state = this.interpretInput(percept)
+    const rule = SimpleReflexAgent.ruleMatch(state, this.rules)
+    const action = rule.action
+    return action
+  }
+
+  interpretInput (percept) {
+    return percept
+  }
+
+  static ruleMatch (state, rules) {
+    for (const rule of rules) {
+      if (rule.condition(...state)) return rule
+    }
+    // fallback if no rule applies
+    return { action: undefined }
+  }
+}
+
+export const reflexVacuumAgent = new SimpleReflexAgent([
+  {
+    condition: ([_location, status]) => status === 'dirty',
+    action: 'suck'
+  },
+  {
+    condition: ([location, _status]) => location === 'A',
+    action: 'right'
+  },
+  {
+    condition: ([location, _status]) => location === 'B',
+    action: 'left'
+  }
+])
--- test.mjs
+++ test.mjs
@@ -1,7 +1,14 @@
 import { strict as assert } from 'assert'
 
 import { tableVacuumAgent } from './intelligentAgents/tableDrivenAgent.mjs'
+import { reflexVacuumAgent } from './intelligentAgents/simpleReflexAgent.mjs'
 
 assert.equal(tableVacuumAgent.action([['A', 'dirty']]), 'suck')
 assert.equal(tableVacuumAgent.action([['A', 'clean']]), 'right')
 assert.equal(tableVacuumAgent.action([['B', 'dirty']]), undefined)
+
+assert.equal(reflexVacuumAgent.action([['A', 'dirty']]), 'suck')
+assert.equal(reflexVacuumAgent.action([['A', 'clean']]), 'right')
+assert.equal(reflexVacuumAgent.action([['B', 'dirty']]), 'suck')
+assert.equal(reflexVacuumAgent.action([['C', 'dirty']]), 'suck')
+assert.equal(reflexVacuumAgent.action([['C', 'clean']]), undefined)
