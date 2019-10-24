fe3f0b341c8cdf710d681a5eb0b93a885a1ca427
David Pomerenke <david.pomerenke@uni-konstanz.de>
2019-10-24 11:09:19 +0100

    add simple problem-driven-agent

--- test.mjs
+++ test.mjs
@@ -2,13 +2,18 @@ import { strict as assert } from 'assert'
 
 import { tableVacuumAgent } from './intelligent-agents/table-driven-agent.mjs'
 import { reflexVacuumAgent } from './intelligent-agents/simple-reflex-agent.mjs'
+import './problem-solving/problem.mjs'
 
+// intelligent agents
+//   table-driven agent
 assert.equal(tableVacuumAgent.action([['A', 'dirty']]), 'suck')
 assert.equal(tableVacuumAgent.action([['A', 'clean']]), 'right')
 assert.equal(tableVacuumAgent.action([['B', 'dirty']]), undefined)
-
+//   simple reflex agent
 assert.equal(reflexVacuumAgent.action([['A', 'dirty']]), 'suck')
 assert.equal(reflexVacuumAgent.action([['A', 'clean']]), 'right')
 assert.equal(reflexVacuumAgent.action([['B', 'dirty']]), 'suck')
 assert.equal(reflexVacuumAgent.action([['C', 'dirty']]), 'suck')
 assert.equal(reflexVacuumAgent.action([['C', 'clean']]), undefined)
+// problem solving
+//   problem
