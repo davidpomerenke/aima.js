b31fd701a95fd380608ddbbc5c08a9f5c60014dc
David Pomerenke <david.pomerenke@uni-konstanz.de>
2019-10-24 13:16:30 +0100

    add eight puzzle

--- problem-solving/problem.mjs
+++ problem-solving/problem.mjs
@@ -1,3 +1,5 @@
+import deepEqual from 'deep-equal'
+
 export class Problem {
   constructor (initialState, model, goalTest) {
     this.state = initialState
@@ -53,4 +55,44 @@ export const vacuumWorld = new Problem(
     }
   },
   state => (state.A === 'clean' && state.B === 'clean')
+)
+
+export const eightPuzzle = new Problem(
+  [
+    [1, 4, 2],
+    [3, 0, 5],
+    [6, 7, 8]
+  ],
+  (state, action) => {
+    const getZeroPosition = state => {
+      const zeroPosition = {}
+      if (state[0][0] === 0 || state[0][1] === 0 || state[0][2] === 0) zeroPosition.y = 0
+      else if (state[1][0] === 0 || state[1][1] === 0 || state[1][2] === 0) zeroPosition.y = 1
+      else if (state[2][0] === 0 || state[2][1] === 0 || state[2][2] === 0) zeroPosition.y = 2
+      if (state[0][0] === 0 || state[1][0] === 0 || state[2][0] === 0) zeroPosition.x = 0
+      else if (state[0][1] === 0 || state[1][1] === 0 || state[2][1] === 0) zeroPosition.x = 1
+      else if (state[0][2] === 0 || state[1][2] === 0 || state[2][2] === 0) zeroPosition.x = 2
+      return zeroPosition
+    }
+    const zeroPosition = getZeroPosition(state)
+    if (action === 'right' && zeroPosition.x < 2) {
+      state[zeroPosition.y][zeroPosition.x] = state[zeroPosition.y][zeroPosition.x + 1]
+      state[zeroPosition.y][zeroPosition.x + 1] = 0
+    } else if (action === 'left' && zeroPosition.x > 0) {
+      state[zeroPosition.y][zeroPosition.x] = state[zeroPosition.y][zeroPosition.x - 1]
+      state[zeroPosition.y][zeroPosition.x - 1] = 0
+    } else if (action === 'down' && zeroPosition.y < 2) {
+      state[zeroPosition.y][zeroPosition.x] = state[zeroPosition.y + 1][zeroPosition.x]
+      state[zeroPosition.y + 1][zeroPosition.x] = 0
+    } else if (action === 'up' && zeroPosition.y > 0) {
+      state[zeroPosition.y][zeroPosition.x] = state[zeroPosition.y - 1][zeroPosition.x]
+      state[zeroPosition.y - 1][zeroPosition.x] = 0
+    }
+    return { state: state, stepCost: 1 }
+  },
+  state => deepEqual(state, [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+  ])
 )
--- test.mjs
+++ test.mjs
@@ -2,7 +2,7 @@ import { strict as assert } from 'assert'
 
 import { tableVacuumAgent } from './intelligent-agents/table-driven-agent.mjs'
 import { reflexVacuumAgent } from './intelligent-agents/simple-reflex-agent.mjs'
-import { vacuumWorld } from './problem-solving/problem.mjs'
+import { vacuumWorld, eightPuzzle } from './problem-solving/problem.mjs'
 
 // intelligent agents
 // -- table-driven agent
@@ -17,6 +17,7 @@ assert.equal(reflexVacuumAgent.action([['C', 'dirty']]), 'suck')
 assert.equal(reflexVacuumAgent.action([['C', 'clean']]), undefined)
 // problem solving
 // -- problem
+// -- -- vacuum world
 assert.deepEqual(vacuumWorld.state, { location: 'A', A: 'dirty', B: 'dirty' })
 vacuumWorld.action('suck')
 assert.deepEqual(vacuumWorld.state, { location: 'A', A: 'clean', B: 'dirty' })
@@ -30,3 +31,23 @@ assert(!vacuumWorld.isSolved)
 vacuumWorld.action('suck')
 assert.deepEqual(vacuumWorld.state, { location: 'B', A: 'clean', B: 'clean' })
 assert(vacuumWorld.isSolved)
+// -- -- eight puzzle
+assert.deepEqual(eightPuzzle.state, [
+  [1, 4, 2],
+  [3, 0, 5],
+  [6, 7, 8]
+])
+eightPuzzle.action('up')
+assert.deepEqual(eightPuzzle.state, [
+  [1, 0, 2],
+  [3, 4, 5],
+  [6, 7, 8]
+])
+assert(!eightPuzzle.isSolved)
+eightPuzzle.action('left')
+assert.deepEqual(eightPuzzle.state, [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8]
+])
+assert(eightPuzzle.isSolved)
