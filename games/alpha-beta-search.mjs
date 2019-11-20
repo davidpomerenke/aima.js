a13cc6f39c710b6f1220a387664b3e6d76435275
David Pomerenke <david.pomerenke@uni-konstanz.de>
2019-11-20 16:42:08 +0000

    improve minimax + alpha-beta search

--- games/alpha-beta-search.mjs
+++ games/alpha-beta-search.mjs
@@ -1,30 +1,38 @@
-// minimax algorithm with alpha-beta pruning
-
-export const alphaBetaSearch = (game, state) =>
+export const alphaBetaSearch = (game, state, limit = Infinity) =>
   game.actions(state)
     .map(action => ({
       action: action,
-      outcome: minValue(game, game.result(state, action))
+      outcome: minValue(game, game.result(state, action), limit - 1, -Infinity, +Infinity)
     }))
     .reduce((current, next) => next.outcome > current.outcome ? next : current)
 
-const maxValue = (game, state, alpha, beta) => {
+export const betaAlphaSearch = (game, state, limit = Infinity) =>
+  game.actions(state)
+    .map(action => ({
+      action: action,
+      outcome: maxValue(game, game.result(state, action), limit - 1, -Infinity, +Infinity)
+    }))
+    .reduce((current, next) => next.outcome < current.outcome ? next : current)
+
+const maxValue = (game, state, limit, alpha, beta) => {
   if (game.terminalTest(state)) return game.utility(state)
+  if (limit < 1) return game.heuristic(state)
   let v = -Infinity
   for (const action of game.actions(state)) {
-    v = Math.max(v, minValue(game, game.result(state, action), alpha, beta))
+    v = Math.max(v, minValue(game, game.result(state, action), limit, alpha, beta))
     if (v >= beta) return v
     alpha = Math.max(alpha, v)
   }
   return v
 }
 
-const minValue = (game, state, alpha, beta) => {
+const minValue = (game, state, limit, alpha, beta) => {
   if (game.terminalTest(state)) return game.utility(state)
+  if (limit < 1) return game.heuristic(state)
   let v = +Infinity
   for (const action of game.actions(state)) {
-    v = Math.min(v, maxValue(game, game.result(state, action), alpha, beta))
-    if (v >= alpha) return v
+    v = Math.min(v, maxValue(game, game.result(state, action), limit, alpha, beta))
+    if (v <= alpha) return v
     beta = Math.min(beta, v)
   }
   return v
--- games/minimax-decision.mjs
+++ games/minimax-decision.mjs
@@ -1,19 +1,31 @@
-export const minimaxDecision = (game, state) =>
+export const minimaxDecision = (game, state, limit = Infinity) =>
   game.actions(state)
     .map(action => ({
       action: action,
-      outcome: minValue(game, game.result(state, action))
+      outcome: minValue(game, game.result(state, action), limit - 1)
     }))
     .reduce((current, next) => next.outcome > current.outcome ? next : current)
 
-const maxValue = (game, state) =>
-  game.terminalTest(state)
-    ? game.utility(state)
-    : game.actions(state).reduce((prev, current) =>
-      Math.max(prev, minValue(game, game.result(state, current))), -Infinity)
+export const maximinDecision = (game, state, limit = Infinity) =>
+  game.actions(state)
+    .map(action => ({
+      action: action,
+      outcome: maxValue(game, game.result(state, action), limit - 1)
+    }))
+    .reduce((current, next) => next.outcome < current.outcome ? next : current)
 
-const minValue = (game, state) =>
+const maxValue = (game, state, limit) =>
   game.terminalTest(state)
     ? game.utility(state)
-    : game.actions(state).reduce((prev, current) =>
-      Math.min(prev, maxValue(game, game.result(state, current))), Infinity)
+    : limit < 1
+      ? game.heuristic(state)
+      : game.actions(state).reduce((prev, current) =>
+        Math.max(prev, minValue(game, game.result(state, current), limit - 1)), -Infinity)
+
+const minValue = (game, state, limit) =>
+  game.terminalTest(state)
+    ? game.utility(state)
+    : limit < 1
+      ? game.heuristic(state)
+      : game.actions(state).reduce((prev, current) =>
+        Math.min(prev, maxValue(game, game.result(state, current), limit - 1)), Infinity)
